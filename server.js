b4f2f19cebb9d48748c3726aee382b45ea39decd
ZhinanLu <Zhinan.Lu@outlook.com>
2023-08-20 16:00:35 -0400

    finish authenticate user setup

--- server.js
+++ server.js
@@ -8,9 +8,11 @@ import mongoose from "mongoose";
 
 // routers
 import jobRouter from "./routes/jobRouter.js";
+import authRouter from "./routes/authRouter.js";
 
 //middleware
 import errorHandlerMiddleware from "./middleware/errorHandlerMiddleware.js";
+import { authenticateUser } from "./middleware/authMiddleware.js";
 
 if (process.env.NODE_ENV === "development") {
   app.use(morgan("dev"));
@@ -21,8 +23,8 @@ app.use(express.json());
 app.get("/", (req, res) => {
   res.send("hello kate");
 });
-
-app.use("/api/v1/jobs", jobRouter);
+app.use("/api/v1/jobs", authenticateUser, jobRouter);
+app.use("/api/v1/auth", authRouter);
 
 app.use("*", (req, res) => {
   res.status(404).json({ msg: "not found" });
