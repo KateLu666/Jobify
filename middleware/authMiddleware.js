72a01260f1eb58a624ad7863efb1e4af5a0e08cc
ZhinanLu <Zhinan.Lu@outlook.com>
2023-08-20 21:08:57 -0400

    finish server

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -1,4 +1,27 @@
-export const authenticateUser = async (req, res, next) => {
-  console.log("auth middleware");
-  next();
+import {
+  UnauthenticatedError,
+  UnauthorizedError,
+} from "../errors/customErrors.js";
+import { verifyJWT } from "../utils/tokenUtils.js";
+
+export const authenticateUser = (req, res, next) => {
+  const { token } = req.cookies;
+  if (!token) throw new UnauthenticatedError("invalid authentication");
+
+  try {
+    const { userId, role } = verifyJWT(token);
+    req.user = { userId, role };
+    next();
+  } catch (error) {
+    throw new UnauthenticatedError("invalid authentication");
+  }
+};
+
+export const authorizePermissions = (...roles) => {
+  return (req, res, next) => {
+    if (!roles.includes(req.user.role)) {
+      throw new UnauthorizedError("unauthorized to access this route");
+    }
+    next();
+  };
 };
--- server.js
+++ server.js
@@ -5,10 +5,12 @@ import express from "express";
 const app = express();
 import morgan from "morgan";
 import mongoose from "mongoose";
+import cookieParser from "cookie-parser";
 
 // routers
 import jobRouter from "./routes/jobRouter.js";
 import authRouter from "./routes/authRouter.js";
+import userRouter from "./routes/userRouter.js";
 
 //middleware
 import errorHandlerMiddleware from "./middleware/errorHandlerMiddleware.js";
@@ -18,13 +20,20 @@ if (process.env.NODE_ENV === "development") {
   app.use(morgan("dev"));
 }
 
+app.use(cookieParser());
 app.use(express.json());
 
 app.get("/", (req, res) => {
   res.send("hello kate");
 });
+
+app.get("/api/v1/test", (req, res) => {
+  res.json({ msg: "test route" });
+});
+
 app.use("/api/v1/jobs", authenticateUser, jobRouter);
 app.use("/api/v1/auth", authRouter);
+app.use("/api/v1/users", authenticateUser, userRouter);
 
 app.use("*", (req, res) => {
   res.status(404).json({ msg: "not found" });
