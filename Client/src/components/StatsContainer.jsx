0157d9869334ebe15b86cbee0fab82ec9dfba918
ZhinanLu <Zhinan.Lu@outlook.com>
2023-08-26 01:09:47 -0400

    Finish Stats Page

---
+++ Client/src/components/StatsContainer.jsx
@@ -0,0 +1,42 @@
+import {
+  FaSuitcaseRolling,
+  FaCalendarCheck,
+  FaWindowClose,
+} from "react-icons/fa";
+import Wrapper from "../assets/wrappers/StatsContainer";
+import StatItem from "./StatItem";
+
+export const StatsContainer = ({ defaultStats }) => {
+  const stats = [
+    {
+      title: "pending applications",
+      count: defaultStats?.pending || 0,
+      icon: <FaSuitcaseRolling />,
+      color: "#f59e0b",
+      bcg: "#fef3c7",
+    },
+    {
+      title: "interviews scheduled",
+      count: defaultStats?.interview || 0,
+      icon: <FaCalendarCheck />,
+      color: "#647acb",
+      bcg: "#e0e8f9",
+    },
+    {
+      title: "jobs declined",
+      count: defaultStats?.declined || 0,
+      icon: <FaWindowClose />,
+      color: "#d66a6a",
+      bcg: "#ffeeee",
+    },
+  ];
+  return (
+    <Wrapper>
+      {stats.map((item) => {
+        return <StatItem key={item.title} {...item} />;
+      })}
+    </Wrapper>
+  );
+};
+
+export default StatsContainer;
--- routes/jobRouter.js
+++ routes/jobRouter.js
@@ -8,6 +8,7 @@ import {
   createJob,
   updateJob,
   deleteJob,
+  showStats,
 } from "../controllers/jobController.js";
 
 import {
@@ -19,6 +20,9 @@ router
   .route("/")
   .get(getAllJobs)
   .post(checkForTestUser, validateJobInput, createJob);
+
+router.route("/stats").get(showStats);
+
 router
   .route("/:id")
   .get(validateIdParam, getJob)
