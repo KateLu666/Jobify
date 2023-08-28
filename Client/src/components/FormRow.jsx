c8a64a84ff9b52272c810cc8559dc8093bcd0986
ZhinanLu <Zhinan.Lu@outlook.com>
2023-08-27 21:50:29 -0400

    Finish All Jobs Page

--- Client/src/components/FormRow.jsx
+++ Client/src/components/FormRow.jsx
@@ -1,5 +1,5 @@
 // eslint-disable-next-line react/prop-types
-const FormRow = ({ type, name, labelText, defaultValue = "" }) => {
+const FormRow = ({ type, name, labelText, defaultValue = "", onChange }) => {
   return (
     <div className="form-row">
       <label htmlFor={name} className="form-label">
@@ -9,8 +9,9 @@ const FormRow = ({ type, name, labelText, defaultValue = "" }) => {
         type={type}
         id={name}
         name={name}
-        defaultValue={defaultValue}
         className="form-input"
+        defaultValue={defaultValue || ""}
+        onChange={onChange}
         required
       />
     </div>
